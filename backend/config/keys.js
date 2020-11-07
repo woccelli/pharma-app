1807eda30030249a5db81343828a9cbdedf88b35
William Occelli <51316463+woccelli@users.noreply.github.com>
2020-11-07 10:54:56 +0100

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI:"mongodb+srv://dbAdmin:zlMsdufhqXBqp8GtOzVt@pharma-app-test-db.bukxk.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI:"",
     
-    secretOrKey: "secret"
+    secretOrKey: ""
 };
