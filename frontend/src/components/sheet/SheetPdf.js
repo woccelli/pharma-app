039085493444d3a35cea5001170406616ad9ea13
woccelli <william.occelli@gmail.com>
2021-06-20 16:39:27 +0200

    Spinner for charging pdf

--- frontend/src/components/sheet/SheetPdf.js
+++ frontend/src/components/sheet/SheetPdf.js
@@ -1,6 +1,7 @@
 import { usePDF } from '@react-pdf/renderer';
 import { useState } from 'react';
 import Sheet from './Sheet'
+import { Box, CircularProgress, Container, Typography } from '@material-ui/core'
 
 const SheetDoc = (sheet, source) => {
     return <Sheet name={sheet.name} address={source} definition={sheet.definition} advices={sheet.advices} sections={sheet.sections} />
@@ -17,7 +18,12 @@ export const SheetPdf = ({sheet: sheetToRender, source}) => {
         updateInstance({ document: doc });
     }
 
-    if (instance.loading) return <div>Chargement ...</div>;
+    if (instance.blob === null) return (
+        <Box alignItems="center" justifyContent="center" style={{width: "100%"}}>
+            <CircularProgress color="secondary"/>
+            <Typography>Chargement ...</Typography>
+        </Box>
+    );
 
     if (instance.error) return <div>Something went wrong: {error}</div>;
 
